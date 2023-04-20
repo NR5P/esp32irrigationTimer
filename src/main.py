73a775671dd3ff7b19ca1976557f751c5d11ebd5
NR5P <orangev8zcar@gmail.com>
2023-04-19 21:09:25 -0500

    .

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from server import Server
 from IrrigatorModel import Irrigator
 from db import DB
+import uasyncio
 
 db = DB()
 
@@ -8,8 +9,14 @@ server = Server()
 #irrigators = db.getIrrigators() # get whats in the json file at startup
 irrigators = [Irrigator("test", "12:30", 30, 30, 3),Irrigator("test", "12:30", 30, 30, 3),Irrigator("test", "12:30", 30, 30, 3)]
 
-while True:
-  server.reConnect() # if lost wifi reconnect
-  server.serve(irrigators) # serve web page if connection pass irrigators to serve data
+async def runIrrigate():
   for irrigator in irrigators: # go through list of irrigators and turn on and off as needed
     irrigator.irrigate()
+
+async def runServer():
+  while True:
+    server.reConnect() # if lost wifi reconnect
+    server.serve(irrigators) # serve web page if connection pass irrigators to serve data
+
+
+
